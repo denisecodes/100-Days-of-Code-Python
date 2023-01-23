5f46b26bacec9ee826143cea7c0a0785d081b28f
Denise Chan <119052310+denisecodes@users.noreply.github.com>
2023-01-23 21:43:59 +0000

    Update main.py
    Updated this file to handle errors, with try and except code

--- Day-26-NATO-Alphabet-Project/main.py
+++ Day-26-NATO-Alphabet-Project/main.py
@@ -1,24 +1,4 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
-
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
 
 #TODO 1. Create a dictionary in this format:
 #{"A": "Alfa", "B": "Bravo"}
@@ -27,8 +7,14 @@ phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-user_word = input("Enter a word: ").upper()
-# Check through dictionary, if letter matches dictionary letter, add NATO alphabet to a list
-output_list = [phonetic_dict[letter] for letter in user_word]
-print(output_list)
-
+check_word = True
+while check_word:
+    user_word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in user_word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+    # Check through dictionary, if letter matches dictionary letter, add NATO alphabet to a list
+    else:
+        print(output_list)
+        check_word = False
