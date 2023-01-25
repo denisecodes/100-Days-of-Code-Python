f2b9d6a3cc837e83481442477ec22bc8dbb9cba9
Denise Chan <119052310+denisecodes@users.noreply.github.com>
2023-01-25 13:48:12 +0000

    Add files via upload

---
+++ Day-33-ISS-Overhead/main.py
@@ -0,0 +1,61 @@
+import requests
+from datetime import datetime
+import smtplib
+import time
+
+MY_LAT = 51.507351 # Your latitude
+MY_LONG = -0.127758 # Your longitude
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+data = response.json()
+
+iss_latitude = float(data["iss_position"]["latitude"])
+iss_longitude = float(data["iss_position"]["longitude"])
+
+#Your position is within +5 or -5 degrees of the ISS position.
+def is_iss_position_close():
+    if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
+        return True
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+
+    if time_now <= sunrise or time_now >= sunset:
+        return True
+
+#If the ISS is close to my current position
+# and it is currently dark
+# Then send me an email to tell me to look up.
+# BONUS: run the code every 60 seconds.
+
+while True:
+    time.sleep(60)
+    if is_iss_position_close() and is_night():
+        my_email = "arielchancodes@gmail.com"
+        password = "bofgyvlirfloytff"
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            # Make connection secure and encrypts email
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(
+                from_addr=my_email,
+                to_addrs=my_email,
+                msg=f"Subject:ISS close to you\n\nThe ISS is above you in the SKy! LOOK UP!"
+            )
+
+
+
+
