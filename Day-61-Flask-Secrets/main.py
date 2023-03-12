274d34c40212f2c6ca355d0eca1acd159cac8fb4
Denise <119052310+denisecodes@users.noreply.github.com>
2023-03-12 21:28:16 +0000

    Add files via upload

---
+++ Day-61-Flask-Secrets/main.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Email, DataRequired, Length
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'abcdefg'
+
+class LoginForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField(label="Log In")
+
+
+app = Flask(__name__)
+app.secret_key = "abcdefg"
+# Import Bootstrap
+Bootstrap(app)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template("login.html", form=login_form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
